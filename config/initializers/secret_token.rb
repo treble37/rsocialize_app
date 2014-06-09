30e71f49a7d13d68e01d3721f03cfe50ce638a6c
Bruce Park <bruce@binarywebpark.com>
2014-06-09 07:15:05 -0700

    remove turbolinks, initial app setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RsocializeApp::Application.config.secret_key_base = '6f025284715577cd4dba22ed22d9ecf11a24345f9a9763d4bb824d0e207065c53e35827334c1f9042f4a79d53513b22298320c1ff1b1e6710d46099f00a46041'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RsocializeApp::Application.config.session_store :cookie_store, key: '_rsocialize_app_session'
